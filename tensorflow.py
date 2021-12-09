0dec161f4d65f8d33736478e05cf76fc1f46d0bc
Jacqueline Lenarz <lenarzjacqueline@gmail.com>
2021-12-09 10:15:13 +0100

    Update tensorflow.py

--- tensorflow.py
+++ tensorflow.py
@@ -1,45 +1,69 @@
-import sys
+#importing dependencies for our kv screen information
+from google.oauth2 import service_account
+from kivy.lang import builder
 
-# Confirm that we're using Python 3
-assert sys.version_info.major == 3, 'Oops, not running Python 3. Use Runtime > Change runtime type'
-
-# TensorFlow and tf.keras
-print("Installing dependencies for Colab environment")
-!pip install -Uq grpcio==1.26.0
-
-import tensorflow as tf
-from tensorflow import keras
-
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
+#importing gsheets dependencies
 import os
-import subprocess
+import sys
+import gspread
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+import oauth2client
+from oauth2client.service_account import ServiceAccountCredentials
+from kivy.metrics import dp
+from kivymd.uix.datatables import MDDataTable
+from kivy.uix.textinput import TextInput
+from pyobjus import autoclass
+import pygsheets
+import pickle
+import os.path
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+#importing ML dependencies
 import pandas as pd
+import tensorflow
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
-#ML libraries
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
+#authorizaiton to read
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file']
+creds = ServiceAccountCredentials.from_json_keyfilename('service_account.json', scope)
+gc = pygsheets.authorize(creds)
 
 
+gc = gspread.authorize(creds)
+sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/13iboRDI7pcjsaWriqcwMvOPd8_d883ZeRlJgnZk4Bn8/edit#gid=1737311642')
+wks = sh.worksheet('Data_Input')
+data = wks.get_all_values()
+headers = data.pop(0)
 
-print('TensorFlow version: {}'.format(tf.__version__))
+df = pd.DataFrame(data, columns=headers)
+ds = df.values
+# split into input (X) and output (Y) variables
+X = ds[:,0:29]
+Y = ds[:,30]
 
+# define base model
+def baseline_model():
+      
+	# create model
+	model = Sequential()
+	model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal'))
+ 
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	return model
 
-
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-# scale the values to 0.0 to 1.0
-train_images = train_images / 255.0
-test_images = test_images / 255.0
-
-# reshape for feeding into the model
-train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
-test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
-
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-print('\ntrain_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
-print('test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
+# evaluate model
+estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=5, verbose=0)
+kfold = KFold(n_splits=10)
+results = cross_val_score(estimator, X, Y, cv=kfold)
+print("Baseline: %.2f (%.2f) MSE" % (results.mean(), results.std()))
