b7da89ff4f44343870d4a2b0f31caff55b72d5ee
Jacqueline Lenarz <lenarzjacqueline@gmail.com>
2021-12-21 00:00:09 +0100

    Update script_gssheetsAPI.py

--- script_gssheetsAPI.py
+++ script_gssheetsAPI.py
@@ -1,27 +1,55 @@
+#importing google dependencies
 import gspread
+from gspread.models import Worksheet
 import googleapiclient
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
+import pygsheets
+from google.auth.transport.requests import Request
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+#importing kv dependencies
+from kivy.uix.screenmanager import Screen, ScreenManager, FallOutTransition
+from kivymd.uix.button import MDFlatButton, MDRaisedButton, MDRectangleFlatButton, ButtonBehavior
+
+#importing classes to work with
+from main import MainApp
+
+
+#authorizing/loading API
+gc = pygsheets.authorize(service_file='/Users/jacquelenarz/Downloads/client_secret.json')
 
 
 
-#setting up our API credentials to read and write
-SCOPES = ['https://googleapis.com/auth/spreadsheets']
-creds = service_account.Credentials.from_service_account_file(
-    'service_account.json', scopes=SCOPES)
-
-SAMPLE_SPREADSHEET_ID = '##not included for data privacy##'
-service = build('sheets', 'v4', credentials=creds)
-
-
-#creating the link to the spreadsheet
-sa = gspread.service_account(filename='service_account.json')
-sh = sa.open('ADHD App API')
+#creating the link to the spreadsheet and creating sheets within
+sh = gc.open('ADHD App API')
+wk_1 = sh.get_worksheet('Data_Input')
+wk_2 = sh.get_worksheet('Diagnosis_Input')
+wk_3 = sh.get_worksheet('Diag_Raw')
+wk_4 = sh.get_worksheet('Diag_Raw_Comnbined')
+wk_5 = sh.get_worksheet('ML_Models')
+wk_6 = sh.get_worksheet('Email')
 
 
-#creating the worksheets
-wk_1 = sh.worksheet("Login_Info")
-wk_2 = sh.worksheet("Data_Info")
-wk_3 = sh.worksheet("Diag_Info")
-wk_4 = sh.worksheet("Diag_ML_Output")
-wk_5 = sh.worksheet("ML_Diag_Combined")
+#opening data input wks to see if we have enough data to run our algo to diagnose respondant
+wk_1.row_count = []
+
+#defining the options for amount of data we have
+def change_screen_no(MainApp, dt):
+    MainApp.root.current = 'notenoughinfo'
+def change_screen_yes(MainApp, dt):
+    MainApp.root.current = 'diagnosis'
+
+#preparing direction of data / diagnosis building
+class Selection(Screen):
+    #creating button to see if enough data has been input to even run the algorithm
+    def change_screen_no(MainApp, dt):
+        MainApp.root.current = 'notenoughinfo'
+    def change_screen_yes(MainApp, dt):
+        MainApp.root.current = 'diagnosis'
+    def diagbtn(MDFlatButton):
+        MDFlatButton.diagbtn = 'diagbutn'
+    def on_press(diagbtn):
+            if wk_1.row_count > 100:
+                change_screen_yes
+            if wk_1.row_count < 100:
+                change_screen_no
